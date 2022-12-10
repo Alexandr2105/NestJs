2e539598d485a1b5d3444757bbdf772367f1dc36
Alexandr2105 <5030553@gmail.com>
2022-12-10 22:48:57 +0300

    new Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,10 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Modules } from './modules';
 
+const mongoUri = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017/tube';
+
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URL),
-    Modules,
-  ],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(mongoUri), Modules],
   controllers: [AppController],
   providers: [AppService],
 })
