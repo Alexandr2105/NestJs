2ed95d39f5167697855c105b2932f3b64fcc6e12
Alexandr2105 <5030553@gmail.com>
2022-12-11 12:59:41 +0300

    new Commit

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -31,7 +31,7 @@ export class PostsService {
           myStatus: 'None',
           newestLikes: [
             {
-              addedAt: new Date().toISOString(),
+              addedAt: '2022-12-10T12:08:15.317Z',
               userId: 'string',
               login: 'string',
             },
