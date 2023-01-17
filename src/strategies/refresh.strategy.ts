e23896cf7f21b7a80e55bca06d6b16643722348a
Alexandr2105 <5030553@gmail.com>
2023-01-17 11:49:25 +0300

    new commit

--- src/strategies/refresh.strategy.ts
+++ src/strategies/refresh.strategy.ts
@@ -22,6 +22,9 @@ export class RefreshStrategy extends PassportStrategy(
   }
 
   validate(payload) {
+    if (payload.exp < new Date()) {
+      return false;
+    }
     return { userId: payload.userId, deviceId: payload.deviceId };
   }
 }
