5e3087536ea607deb6598c8812d5d68328797fc9
Alexandr2105 <5030553@gmail.com>
2023-01-18 11:02:08 +0300

    new commit

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   // app.set('trust proxy', true);
   app.enableCors();
   app.useGlobalPipes(
@@ -35,6 +34,7 @@ async function bootstrap() {
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port);
+  app.use(cookieParser());
 }
 
 bootstrap();
