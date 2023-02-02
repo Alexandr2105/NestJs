631a8b5bbfe6d114ba19c9115aa9b9acfc9674ca
Alexandr2105 <5030553@gmail.com>
2023-02-02 17:28:39 +0300

    new comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Modules } from './modules';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Blog } from './features/public/blogs/schema/blogs.schema';
-import { Post } from './features/public/posts/schema/posts.schema';
-import { User } from './features/sa/users/schema/user';
-import { Comment } from './features/public/comments/schema/comment.schema';
-import { LikesModel } from './common/schemas/like.type.schema';
-import { EmailConfirmation } from './common/schemas/email.confirmation.schema';
-import { RefreshTokenData } from './common/schemas/refresh.token.data.schema';
-import { CountAttempt } from './common/schemas/count.attempt.schema';
-import { BanUser } from './features/sa/users/schema/banUser';
 
 const mongoUri = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017/tube';
 
@@ -22,17 +12,17 @@ const mongoUri = process.env.MONGO_URL || 'mongodb://0.0.0.0:27017/tube';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(mongoUri),
     Modules,
-    TypeOrmModule.forFeature([
-      Blog,
-      Post,
-      User,
-      Comment,
-      LikesModel,
-      EmailConfirmation,
-      RefreshTokenData,
-      CountAttempt,
-      BanUser,
-    ]),
+    // TypeOrmModule.forFeature([
+    //   Blog,
+    //   Post,
+    //   User,
+    //   Comment,
+    //   LikesModel,
+    //   EmailConfirmation,
+    //   RefreshTokenData,
+    //   CountAttempt,
+    //   BanUser,
+    // ]),
   ],
   controllers: [AppController],
   providers: [AppService],
