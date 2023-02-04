1ca309be34677c0183990f16bd24088c897d508a
Alexandr2105 <5030553@gmail.com>
2023-02-04 11:58:16 +0300

    new comment

--- src/features/public/queryReposytories/query.repository.ts
+++ src/features/public/queryReposytories/query.repository.ts
@@ -417,7 +417,7 @@ export class QueryRepository {
             description: a.description,
             websiteUrl: a.websiteUrl,
             createdAt: a.createdAt,
-            isMembership: true,
+            isMembership: false,
             blogOwnerInfo: {
               userId: user.id,
               userLogin: user.login,
