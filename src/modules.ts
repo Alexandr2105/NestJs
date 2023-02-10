fe04a6459d9c516fafc8263e4c3a979e94c5450e
Alexandr2105 <5030553@gmail.com>
2023-02-10 15:11:43 +0300

    new comment

--- src/modules.ts
+++ src/modules.ts
@@ -286,7 +286,7 @@ const AbstractClassesMongo = [
     ...UseCases,
     ...SqlRepositories,
     ...MongoRepositories,
-    ...AbstractClassesMongo,
+    ...AbstractClassesSql,
   ],
 })
 export class Modules {}
