84b85a14e88242b14d2ddd284b01463af356390a
Alexandr2105 <5030553@gmail.com>
2023-02-11 18:43:55 +0300

    new comment

--- src/features/public/queryReposytories/query.repository.sql.ts
+++ src/features/public/queryReposytories/query.repository.sql.ts
@@ -11,13 +11,19 @@ import {
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { QueryCount } from '../../../common/helper/query.count';
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { ICommentsRepository } from '../comments/i.comments.repository';
 
 @Injectable()
 export class QueryRepositorySql extends IQueryRepository {
   constructor(
     @InjectDataSource() private readonly dataSource: DataSource,
     private readonly queryCount: QueryCount,
+    private readonly commentsRepository: ICommentsRepository,
   ) {
     super();
   }
@@ -107,45 +113,172 @@ export class QueryRepositorySql extends IQueryRepository {
     };
   }
 
-  getQueryAllBannedUsersForBlog(
+  async getQueryAllBannedUsersForBlog(
     query: any,
     blogId: string,
     ownerId: string,
   ): Promise<BanUsersInfoForBlog> {
-    return Promise.resolve(undefined);
+    const blog = await this.dataSource.query(
+      `SELECT * FROM public."Blogs"
+            WHERE "id"=$1`,
+      [blogId],
+    );
+    if (!blog[0]) throw new NotFoundException();
+    if (blog[0].userId !== ownerId) throw new ForbiddenException();
+    const banUsers = await this.dataSource.query(
+      `SELECT * FROM public."BanUsers"
+            WHERE "blogId"=$1`,
+      [blog[0].id],
+    );
+    const totalCount = await this.dataSource.query(
+      `SELECT count(*) FROM public."Users"
+            WHERE "id"=$1 AND "login"=$2`,
+      [
+        banUsers.map((a) => {
+          return a.userId;
+        }),
+        `%${query.searchLoginTerm}%`,
+      ],
+    );
+    const banUsersArraySort = await this.dataSource.query(
+      `SELECT * FROM public."Users"
+            WHERE "id"=$1 AND "login"=$2
+            ORDER BY "${query.sortBy}" ${query.sortDirection}
+            LIMIT $3 OFFSET $4`,
+      [
+        banUsers.map((a) => {
+          return a.userId;
+        }),
+        `%${query.searchLoginTerm}%`,
+        query.pageSize,
+        this.queryCount.skipHelper(query.pageNumber, query.pageSize),
+      ],
+    );
+    return {
+      pagesCount: this.queryCount.pagesCountHelper(
+        totalCount.count,
+        query.pageSize,
+      ),
+      page: query.pageNumber,
+      pageSize: query.pageSize,
+      totalCount: +totalCount.count,
+      items: banUsersArraySort.map((a) => {
+        const banInfo = banUsers.find((b) => a.id === b.userId);
+        return {
+          id: a.id,
+          login: a.login,
+          banInfo: {
+            isBanned: banInfo.isBanned,
+            banDate: banInfo.banDate,
+            banReason: banInfo.banReason,
+          },
+        };
+      }),
+    };
   }
 
-  getQueryAllInfoForBlog(
+  async getQueryAllInfoForBlog(
     query: any,
     userId: string,
   ): Promise<AllCommentsForAllPostsCurrentUserBlogs> {
+    const arrayPosts = await this.dataSource.query(
+      `SELECT * FROM public."Blogs"
+            WHERE "userId"=$1`,
+      [userId],
+    );
+    const totalCount = await this.dataSource.query(
+      `SELECT count(*) FROM public."Comments"
+            WHERE "idPost"=$1`,
+      [
+        arrayPosts.map((a) => {
+          return a.id;
+        }),
+      ],
+    );
+    const sortArrayComments = await this.dataSource.query(
+      `SELECT * FROM public."Comments"
+            WHERE "idPost"=$1
+            ORDER BY "${query.sortBy}" ${query.sortDirection}
+            LIMIT $2 OFFSET $3`,
+      [
+        arrayPosts.map((a) => {
+          return a.id;
+        }),
+        query.pageSize,
+        this.queryCount.skipHelper(query.pageNumber, query.pageSize),
+      ],
+    );
+    return {
+      pagesCount: this.queryCount.pagesCountHelper(
+        totalCount.count,
+        query.pageSize,
+      ),
+      page: query.pageNumber,
+      pageSize: query.pageSize,
+      totalCount: totalCount,
+      items: await Promise.all(
+        sortArrayComments.map(async (a) => {
+          const comment = arrayPosts.find((b) => a.idPost === b.id);
+          const likeInfo = await this.commentsRepository.getLikesInfo(a.userId);
+          const dislikeInfo = await this.commentsRepository.getDislikeInfo(
+            a.userId,
+          );
+          const myStatus = await this.commentsRepository.getMyStatus(
+            a.userId,
+            a.id,
+          );
+          return {
+            id: a.id,
+            content: a.content,
+            commentatorInfo: {
+              userId: a.userId,
+              userLogin: a.userLogin,
+            },
+            createdAt: a.createdAt,
+            postInfo: {
+              id: comment.id,
+              title: comment.title,
+              blogId: comment.blogId,
+              blogName: comment.blogName,
+            },
+            likesInfo: {
+              likesCount: likeInfo,
+              dislikesCount: dislikeInfo,
+              myStatus: myStatus,
+            },
+          };
+        }),
+      ),
+    };
+  }
+
+  async getQueryBlogs(query: any): Promise<BlogsQueryType> {
     return Promise.resolve(undefined);
   }
 
-  getQueryBlogs(query: any): Promise<BlogsQueryType> {
+  async getQueryBlogsAuthUser(
+    query: any,
+    userId: string,
+  ): Promise<BlogsQueryType> {
     return Promise.resolve(undefined);
   }
 
-  getQueryBlogsAuthUser(query: any, userId: string): Promise<BlogsQueryType> {
+  async getQueryBlogsSA(query: any): Promise<BlogsQueryTypeSA> {
     return Promise.resolve(undefined);
   }
 
-  getQueryBlogsSA(query: any): Promise<BlogsQueryTypeSA> {
-    return Promise.resolve(undefined);
-  }
-
-  getQueryCommentsByPostId(
+  async getQueryCommentsByPostId(
     query: any,
     postId: string,
   ): Promise<CommentsType | boolean> {
     return Promise.resolve(undefined);
   }
 
-  getQueryPosts(query: any, userId: string): Promise<PostQueryType> {
+  async getQueryPosts(query: any, userId: string): Promise<PostQueryType> {
     return Promise.resolve(undefined);
   }
 
-  getQueryPostsBlogsId(
+  async getQueryPostsBlogsId(
     query: any,
     blogId: string,
     userId: string,
