376cc1fc038069cced5bc9606f4584325541fd26
Alexandr2105 <5030553@gmail.com>
2023-02-27 16:33:53 +0300

    new comment

--- src/features/sa/users/i.users.repository.ts
+++ src/features/sa/users/i.users.repository.ts
@@ -1,15 +1,14 @@
-import { User, UserDocument } from './schema/user';
-import { EmailConfirmationDocument } from '../../../common/schemas/email.confirmation.schema';
+import { User } from './schema/user';
 import { EmailResending } from '../../public/auth/dto/auth.dto';
 import { BanUsersDocument } from './schema/banUsers';
 
 export abstract class IUsersRepository {
-  abstract deleteUser(id: string): Promise<boolean>;
-  abstract findLoginOrEmail(logOrEmail: string): Promise<UserDocument>;
-  abstract getUserId(id: string): Promise<UserDocument | false>;
-  abstract updateEmailConfirmation(id: string): Promise<boolean>;
-  abstract getUserByCode(code: string): Promise<EmailConfirmationDocument>;
-  abstract setConfirm(body: EmailResending, newCode: string): Promise<boolean>;
+  abstract deleteUser(id: string);
+  abstract findLoginOrEmail(logOrEmail: string);
+  abstract getUserId(id: string);
+  abstract updateEmailConfirmation(id: string);
+  abstract getUserByCode(code: string);
+  abstract setConfirm(body: EmailResending, newCode: string);
   abstract getUserByIdAll(id: string);
   abstract getBanUsers();
   abstract deleteBanUsers(userId: string);
--- src/features/sa/users/users.repository.typeorm.ts
+++ src/features/sa/users/users.repository.typeorm.ts
@@ -1,47 +1,113 @@
 import { IUsersRepository } from './i.users.repository';
 import { BanUsersDocument } from './schema/banUsers';
 import { EmailResending } from '../../public/auth/dto/auth.dto';
-import { User, UserDocument } from './schema/user';
-import { EmailConfirmationDocument } from '../../../common/schemas/email.confirmation.schema';
+import { User } from './schema/user';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entity/user.entity';
+import { EmailConfirmationEntity } from '../../../common/entity/email.confirmation.entity';
+import { BanUsersEntity } from './entity/banUsers.entity';
+import { add } from 'date-fns';
 
 export class UsersRepositoryTypeorm extends IUsersRepository {
-  async deleteBanUsers(userId: string) {}
+  constructor(
+    private readonly usersCollection: Repository<UserEntity>,
+    private readonly registrationUsersCollection: Repository<EmailConfirmationEntity>,
+    private readonly banUsers: Repository<BanUsersEntity>,
+  ) {
+    super();
+  }
+  async deleteBanUsers(userId: string) {
+    await this.banUsers.delete({ userId: userId });
+  }
 
   async deleteUser(id: string): Promise<boolean> {
-    return Promise.resolve(false);
+    const result = await this.usersCollection.delete({ id: id });
+    return result.affected === 1;
   }
 
-  async findLoginOrEmail(logOrEmail: string): Promise<UserDocument> {
-    return Promise.resolve(undefined);
+  async findLoginOrEmail(logOrEmail: string): Promise<UserEntity> {
+    return this.usersCollection.findOneBy([
+      { login: logOrEmail },
+      { email: logOrEmail },
+    ]);
   }
 
-  async getBanUser(userId: string) {}
-
-  async getBanUsers() {}
-
-  async getConfByUserId(userId: string) {}
-
-  async getUserByCode(code: string): Promise<EmailConfirmationDocument> {
-    return Promise.resolve(undefined);
+  async getBanUser(userId: string) {
+    return this.banUsers.findOneBy({ userId: userId });
   }
 
-  async getUserByEmail(email: string) {}
-
-  async getUserByIdAll(id: string) {}
-
-  async getUserId(id: string): Promise<UserDocument | false> {
-    return Promise.resolve(undefined);
+  async getBanUsers() {
+    return this.usersCollection.findBy({ ban: true });
   }
 
-  async save(user: User) {}
+  async getConfByUserId(userId: string) {
+    return this.registrationUsersCollection.findOneBy({ userId: userId });
+  }
 
-  async saveBan(banInfo: BanUsersDocument) {}
+  async getUserByCode(code: string): Promise<EmailConfirmationEntity> {
+    return this.registrationUsersCollection.findOneBy({
+      confirmationCode: code,
+    });
+  }
+
+  async getUserByEmail(email: string) {
+    return this.usersCollection.findOne({
+      where: { email: email },
+      select: { id: true, login: true, email: true, createdAt: true },
+    });
+  }
+
+  async getUserByIdAll(id: string) {
+    return this.usersCollection.findOneBy({ id: id });
+  }
+
+  async getUserId(id: string): Promise<UserEntity | false> {
+    const user = await this.usersCollection.findOne({
+      where: { id: id },
+      select: { id: true, login: true, email: true, createdAt: true },
+    });
+    if (!user) return false;
+    return user;
+  }
+
+  async save(user: User) {
+    await this.usersCollection.save(user);
+  }
+
+  async saveBan(banInfo: BanUsersDocument) {
+    await this.banUsers.save(banInfo);
+  }
 
   async setConfirm(body: EmailResending, newCode: string): Promise<boolean> {
-    return Promise.resolve(false);
+    const user: any = await this.usersCollection.findOneBy({
+      email: body.email,
+    });
+    const checkUserEmailConfirmation =
+      await this.registrationUsersCollection.findOneBy({
+        userId: user?.id,
+      });
+    if (checkUserEmailConfirmation) {
+      const result = await this.registrationUsersCollection.update(
+        { userId: user?.id },
+        { confirmationCode: newCode },
+      );
+      return result.affected === 1;
+    } else {
+      await this.registrationUsersCollection.create({
+        userId: user?.id,
+        confirmationCode: newCode,
+        expirationDate: add(new Date(), { hours: 1 }),
+        isConfirmed: false,
+      });
+      return true;
+    }
   }
 
   async updateEmailConfirmation(id: string): Promise<boolean> {
-    return Promise.resolve(false);
+    const result = await this.registrationUsersCollection.update(
+      { userId: id },
+      { isConfirmed: true },
+    );
+    return result.affected === 1;
   }
 }
