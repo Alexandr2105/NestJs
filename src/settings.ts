11a6bb9bd2e0a1a5fdc4f656f99a4870e7ed80c9
Alexandr2105 <5030553@gmail.com>
2023-03-07 15:43:01 +0300

    new comment

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   JWT_SECRET: process.env.JWT_SECRET || '1234',
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || '12345',
-  TOKEN_LIFE: 5,
-  REFRESH_TOKEN_LIFE: 10,
+  TOKEN_LIFE: 10,
+  REFRESH_TOKEN_LIFE: 20,
 };
