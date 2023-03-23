44e3f2e36bd57746505cf741e0c53b251289833f
Alexandr2105 <5030553@gmail.com>
2023-03-23 14:49:37 +0300

    new comment

--- src/modules.ts
+++ src/modules.ts
@@ -413,7 +413,7 @@ const entities = [
     ...SqlRepositories,
     ...MongoRepositories,
     ...TypeOrmRepositories,
-    ...AbstractClassesTypeorm,
+    ...AbstractClassesSql,
   ],
 })
 export class Modules {}
