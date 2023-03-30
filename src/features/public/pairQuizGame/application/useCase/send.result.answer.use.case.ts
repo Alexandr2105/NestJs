8838252ca2e7e412f11fd3c90fda4193bba3db74
Alexandr2105 <5030553@gmail.com>
2023-03-30 17:02:19 +0300

    new comment

--- src/features/public/pairQuizGame/application/useCase/send.result.answer.use.case.ts
+++ src/features/public/pairQuizGame/application/useCase/send.result.answer.use.case.ts
@@ -183,6 +183,6 @@ export class SendResultAnswerUseCase {
       gameInfo.finishGameDate = new Date().toISOString();
       await this.gamesRepository.save(gameInfo);
       await this.saveStatistic(gameInfo);
-    }, 4000);
+    }, 10000);
   }
 }
